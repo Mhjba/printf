9dcdf086fc97766dd6a85a26b2f95a94a66c02bf
Mhjba <mahjoubaaitalla15@gmail.com>
2023-12-29 12:38:24 -0800

    printf

--- printf.c
+++ printf.c
@@ -1,34 +1,104 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * _printf - implementation of the inbuilt printf
- * @format: the format specifier
- * Return: the formated string
+ * print_char - ...
+ * @pa: pointer to argument
+ * Return: int
+ */
+
+int print_char(va_list pa)
+{
+char x;
+
+x = va_arg(pa, int);
+_putchar(x);
+return (1);
+}
+
+/**
+ * print_string - ...
+ * @pa: pointer to argument
+ * Return: counter
+ */
+
+int print_string(va_list pa)
+{
+char *str;
+int i = 0;
+
+str = va_arg(pa, char *);
+
+if (str == NULL)
+str = "(null)";
+
+while (str[i])
+{
+_putchar(str[i]);
+i++;
+}
+
+return (i);
+}
+
+/**
+ * print_percentage - ...
+ * @pa: pointer to argument
+ * Return: nothing
+ */
+
+int print_percentage(va_list pa)
+{
+(void) pa;
+_putchar('%');
+return (1);
+}
+
+/**
+ * _printf - work as printf function
+ * @format: pointer to argument
+ * @...: ...
+ * Return: (int)
  */
 
 int _printf(const char *format, ...)
 {
-	int printed = 0;
+va_list pa;
+int i = 0, count = 0;
 
-	va_list args;
+va_start(pa, format);
 
-	va_start(args, format);
 
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-			printed = selector(format, args, printed);
-			format++;
-		}
-		else
-		{
-			_putchar(*format);
-			printed++;
-			format++;
-		}
-	}
-	va_end(args);
-	return (printed);
+if (format == NULL)
+return (-1);
+
+while (format[i])
+{
+if (format[i] != '%')
+{
+_putchar(format[i]);
+count++;
+}
+else
+{
+switch (format[i + 1])
+{
+case 'c':
+count += print_char(pa);
+break;
+case 's':
+count += print_string(pa);
+break;
+case '%':
+count += print_percentage(pa);
+break;
+default:
+return (-1);
+}
+i++;
+}
+i++;
+}
+
+va_end(pa);
+return (count);
 }
