090a9db7c860f2aa2350ce514e2a07db76680de1
Mhjba <mahjoubaaitalla15@gmail.com>
2023-12-29 11:47:56 -0800

    printf

--- fun.c
+++ fun.c
@@ -45,13 +45,3 @@ void handle_octal(unsigned int num, size_t *count)
 	print_octal(num, count);
 }
 
-/**
- * handle_hex - entry point
- * @num: the number
- * @is_upper: 0 if false, 1 if true
- * @count: the number count
- */
-void handle_hex(unsigned int num, int is_upper, size_t *count)
-{
-	print_hex(num, is_upper, count);
-}
